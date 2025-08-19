d1d8f75bd0c8d7aebb5b56f95cbcb19befc7f2a7
rainyxeon@gmail.com <rainyxeon@gmail.com>
2025-08-19 17:53:55 +0700

    KernelSU: integrate scope-minimized manual hooks Thanks to backslashxx for this commit This refactors original KSU hooks to replace deep kernel function hooks with targeted hooks. This backports KernelSU pr#1657 and having pr#2084 elements (32-bit sucompat). It reduces the scope of kernel function interception and still maintains full fucntionality.
    This commit is a squash of the following:
    *	fs/exec: sys_execve: ksu_handle_execve_ksud/sucompat hook
    *	fs/exec: compat_sys_execve: ksu_handle_execve_sucompat hook
    	fs/open: sys_faccessat: ksu_handle_faccessat hook
    *	fs/read_write: sys_read: ksu_handle_sys_read hook
    *	fs/stat: sys_newfstatat: ksu_handle_stat hook
    *	fs/stat: sys_fstatat64: ksu_handle_stat hook
    *	drivers: input: input_event: ksu_handle_input_handle_event hook
    *	drivers: tty/pty.c: pts_unix98_lookup: ksu_handle_devpts hook
    
    references: KernelSU pr#1657, pr#2084
    	https://kernelsu.org/guide/how-to-integrate-for-non-gki.html
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- fs/read_write.c
+++ fs/read_write.c
@@ -429,18 +429,10 @@ ssize_t kernel_read(struct file *file, void *buf, size_t count, loff_t *pos)
 }
 EXPORT_SYMBOL(kernel_read);
 
-#ifdef CONFIG_KSU
-extern bool ksu_vfs_read_hook __read_mostly;
-extern int ksu_handle_vfs_read(struct file **file_ptr, char __user **buf_ptr,
-			size_t *count_ptr, loff_t **pos);
-#endif
 ssize_t vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 {
 	ssize_t ret;
-#ifdef CONFIG_KSU 
-	if (unlikely(ksu_vfs_read_hook))
-		ksu_handle_vfs_read(&file, &buf, &count, &pos);
-#endif
+
 	if (!(file->f_mode & FMODE_READ))
 		return -EBADF;
 	if (!(file->f_mode & FMODE_CAN_READ))
@@ -575,6 +567,12 @@ static inline void file_pos_write(struct file *file, loff_t pos)
 	if ((file->f_mode & FMODE_STREAM) == 0)
 		file->f_pos = pos;
 }
+
+#ifdef CONFIG_KSU
+ extern bool ksu_vfs_read_hook __read_mostly;
+ extern int ksu_handle_sys_read(unsigned int fd, char __user **buf_ptr,
+ 			size_t *count_ptr);
+#endif
 
 #ifdef CONFIG_KSU
  extern bool ksu_vfs_read_hook __read_mostly;
